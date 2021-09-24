38e96865ac536e5fc30ec1c268c9acc9c7fea0af
Adrika-Singhal <77188343+Adrika-Singhal@users.noreply.github.com>
2021-09-24 17:55:21 +0530

    Add files via upload

--- letschat.js
+++ letschat.js
@@ -10,35 +10,3 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-function add_room()
-{
-      Room_names = document.getElementById("room").value;
-
-      firebase.database().ref("/").child(Room_names).update({
-      purpose : "adding room name"            
-      });
-
-      localStorage.setItem("Room_name", Room_names);
-
-      window.location = "kwitter_page.html";
-}
-
-function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-  Room_names = childKey;
- //Start code
- console.log("Room name - " + Room_names);
- row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomNamw(this.id)' >#" + Room_names +"</div><hr>";
- document.getElementById("output").innerHTML +=row;
- //End code
- });});}
-getData();
-
-
-function redirectToRoomName(name)
-{
-      console.log(name);
-      localStorage.setItem("Room_name", name);
-       window.location = "kwitter_page.html"
-
-}
